f23f5eca8d50b1ae07a4834f0091d4de1352068a
miguel-anton <miguelantonortiz@gmail.com>
2025-07-09 11:00:22 +0200

    ajustes de simbiosis entre laravel y angular

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/{any}', function (Request $request) {
+    // Proxy a SSR de Angular (ajusta el puerto si es necesario)
+    $ssrUrl = 'http://localhost:4000' . $request->getRequestUri();
+
+    // Pasa los headers del request original
+    $response = Http::withHeaders($request->headers->all())->get($ssrUrl);
+
+    // Devuelve la respuesta del SSR de Angular
+    return response($response->body(), $response->status())
+        ->header('Content-Type', $response->header('Content-Type'));
+})->where('any', '.*');
