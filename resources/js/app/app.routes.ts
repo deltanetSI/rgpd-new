ce1cd10b9d18e529573989af48b2a4d4ce7aade7
miguel-anton <miguelantonortiz@gmail.com>
2025-07-09 15:57:57 +0200

    ajustillos

--- resources/js/app/app.routes.ts
+++ resources/js/app/app.routes.ts
@@ -1,23 +1,3 @@
 import { Routes } from '@angular/router';
-import { authGuard } from './auth/auth.guard';
-import { roleGuard } from './auth/role.guard';
-import { permissionGuard } from './auth/permission.guard';
 
-export const routes: Routes = [
-  {
-    path: 'dashboard',
-    canActivate: [authGuard],
-    loadComponent: () => import('./dashboard/dashboard.component').then(m => m.DashboardComponent),
-  },
-  {
-    path: 'admin',
-    canActivate: [roleGuard('admin')],
-    loadComponent: () => import('./admin/admin.component').then(m => m.AdminComponent),
-  },
-  {
-    path: 'articles/edit',
-    canActivate: [permissionGuard('edit articles')],
-    loadComponent: () => import('./articles/edit-article.component').then(m => m.EditArticleComponent),
-  },
-  // ...otras rutas...
-];
+export const routes: Routes = [];
