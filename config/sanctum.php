e361393f506276146e270ba56731b1d9c7214b4e
miguel-anton <miguelantonortiz@gmail.com>
2025-07-17 17:50:12 +0200

    configuraciones

--- config/sanctum.php
+++ config/sanctum.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'localhost,localhost:4200,127.0.0.1,127.0.0.1:4200')),
+    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'localhost,localhost:4200,127.0.0.1,127.0.0.1:4200,https://pre.regpdnet.com')),
 
     /*
     |--------------------------------------------------------------------------
