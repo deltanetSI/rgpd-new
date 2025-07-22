ad09604f5b0b85f3b2d19f1e884f849b18d6769c
miguel-anton <miguelantonortiz@gmail.com>
2025-07-22 10:42:19 +0200

    arreglado

--- resources/js/app/app.ts
+++ resources/js/app/app.ts
@@ -4,6 +4,8 @@ import { PrimeNG } from 'primeng/config';
 import { AuthService } from './auth/services/auth.service';
 import { ProgressSpinnerModule } from 'primeng/progressspinner';
 import { AsyncPipe } from '@angular/common';
+import { Observable, combineLatest, timer } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -16,9 +18,21 @@ export class App implements OnInit {
   private primeng = inject(PrimeNG);
   public authService = inject(AuthService);
 
+  // Nuevo observable para controlar la visibilidad
+  showContent$: Observable<boolean>;
+
+  constructor() {
+    // Combinamos el estado de autenticación con un temporizador de 1 segundo
+    this.showContent$ = combineLatest([
+      this.authService.isAuthStatusChecked$,
+      timer(1000) // Espera al menos 1000 ms
+    ]).pipe(
+      // Solo emitimos true cuando ambos observables han emitido
+      map(([isAuthChecked]) => isAuthChecked)
+    );
+  }
 
   ngOnInit() {
-   
     this.primeng.ripple.set(true);
   }
 }
